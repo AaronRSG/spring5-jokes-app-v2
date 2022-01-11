ced65a476ea927e5e3515b0c2923ca7873fa2456
AaronRSG <aaronsgamble@gmail.com>
2022-01-11 09:47:12 +0000

    My Solution

---
+++ spring5-jokes-app-v2/src/main/java/guru/springframework/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package guru.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.services.JokeService;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping("/")
+    public String getJoke(Model model){
+        model.addAttribute("joke", jokeService.getRandomQuote());
+        return "index";
+    }
+    
+}
---
+++ spring5-jokes-app-v2/src/main/java/guru/springframework/services/JokeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.services;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Service
+public class JokeService extends ChuckNorrisQuotes{
+}
